89e4cc76fc911357760ccaf7d7aa238afec2908f
jcrowe91 <jcrowe9119@gmail.com>
2022-03-20 19:47:15 -0500

    Added overview

--- AlarmClockProject/Program.cs
+++ AlarmClockProject/Program.cs
@@ -2,11 +2,23 @@
 
 namespace AlarmClockProject
 {
+    // user will pass in a time in the console
+    // parse the time
+    // start timer
+    // alert user after time up
     class Program
     {
         static void Main(string[] args)
         {
-           
+            var userTime = (args[0]);
+            var parseTime = DateTime.Parse(userTime);
+            var current = DateTime.Now;
+
+            Console.WriteLine(parseTime);
+            Console.WriteLine(current);
+
+                      
+            
         }
     }
 }
