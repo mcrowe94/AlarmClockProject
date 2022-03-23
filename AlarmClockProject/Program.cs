f16270f1da0a73efdb084c4ce26cfcaceb62e556
jcrowe91 <jcrowe9119@gmail.com>
2022-03-23 17:56:07 -0500

    Finished basic flags

--- AlarmClockProject/Program.cs
+++ AlarmClockProject/Program.cs
@@ -10,10 +10,7 @@ namespace AlarmClockProject
     class Program
     {
         static void Main(string[] args)
-        {
-            
-            //var stopWatch = new Stopwatch();
-            
+        {                               
 
             if (args[0] == "--alarm")
             {
@@ -23,8 +20,34 @@ namespace AlarmClockProject
 
                 if(DateTime.Now.Hour == parseTime.Hour && DateTime.Now.Minute == parseTime.Minute)
                 {
-                    //play Youtube
+                    Console.WriteLine("ALARM");
                 }
+            }
+            else if (args[0] == "--stopwatch")
+            {                
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+
+                for (int i = 0; i <= 10000; i++)
+                {
+                    Console.WriteLine(stopWatch.Elapsed);
+                }
+            }
+            else if (args[0] == "--time")
+            {                
+                var time = DateTime.Now;
+                Console.WriteLine(time);
+            }
+            else if (args[0] == "--help")
+            {
+                Console.WriteLine("--time for current time" +
+                    "\n--alarm plus time to go off for alarm function" +
+                    "\n--stopwatch for stopwatch function" +
+                    "\n--help for help options");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Argument");
             }
 
 
